756fdc7d5b490e0c570b5499b5841f536d261da2
rajan tiwari <rajan@instaacoders.com>
2020-01-27 14:27:03 +0530

    Cockpit and server component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'server-app';
+ serverElements = [{type: 'server', name: 'Testserver', content : 'Just a test !' }];
 }
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    // this.severElements.push({
+    //   type: 'server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // )};
+
+   
+  }
+
+  onAddblueprint() {
+    // this.severElements.push({
+    //   type: 'blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // )};
+
+  }
+
+}
