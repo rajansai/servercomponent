59d6242ff62b475109457b033c6420ada45a9bd0
Rajan Tiwari <rajankumar.sai@gmail.com>
2020-01-28 23:58:56 +0530

    Sever component Modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  serverElements = [{type:'server', name:'Testserver', content:'Just a test'}];
+
+ onServerAdded(serverData:{serverName:string, serverContent:string}) {
+  this.severElements.push({
+    type: 'server',
+    name: serverData.serverName,
+    content: serverData.serverContent
+  )};
+
+ 
+}
+
+onBlueprintAdded() {
+  this.severElements.push({
+    type: 'blueprint',
+    name: this.newServerName,
+    content: this.newServerContent
+  )};
+
+}
+
 }
