15e1e3df7a1ef76f54c1bc7fe61d53fe2bdc8abc
Rajan Tiwari <rajankumar.sai@gmail.com>
2020-01-28 15:35:24 +0530

    Forms Module Added

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  newServerName = '';
+  newServerContent = '';
+
   constructor() { }
 
   ngOnInit() {
